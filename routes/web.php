edb84f02c70ee1a026c33fd2aed6bc7fddb85ca1
ElisaSalvatore <elisa.salvatore110@gmail.it>
2022-03-08 17:50:18 +0100

    Mods Routes name

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', "HomeController@index")->name("home");
 
 // Route::resource("comics", "ComicController");
-Route::get("branches", "BranchController@index")->name("branches.index");
-Route::get("branches/{branch}", "BranchController@show")->name("branches.show");
-Route::get("branches/create", "BranchController@create")->name("branches.create");
+Route::get("comics", "ComicController@index")->name("comics.index");
+Route::get("comics/{comic}", "ComicController@show")->name("comics.show");
+Route::get("comics/create", "ComicController@create")->name("comics.create");
